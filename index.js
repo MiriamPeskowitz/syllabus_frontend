0c3c641b82f76cf1fd82ca848234325290d0c57b
MiriamPeskowitz <miriam5000@gmail.com>
2020-07-29 17:39:41 -0400

    Handles post request pt 1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	getSyllabi()
 
 	const createSyllabusForm = document.querySelector("#create-syllabus-form")
-	createSyllabusForm.addEventListener("submit", (e) => createFormHandler(e))
+	createSyllabusForm.addEventListener("submit", (e) => createFormHandler(e))  // grab th einputs with e so that below, you can parse them to send 
 })
 
 function getSyllabi() {
@@ -30,9 +30,45 @@ function getSyllabi() {
 }
 
 function createFormHandler(e) {
-	e.preventDefault()
-	console.log(e)
+  e.preventDefault()
+  const titleInput = document.querySelector('#input-title').value
+  const descriptionInput = document.querySelector('#input-description').value
+  const imageInput = document.querySelector('#input-url').value
+  const categoryInput = document.querySelector('#categories').value
+  //because categoryInput is a string and needs to be an integer for the backend 
+  const categoryId = parseInt(categoryInput)
+  postSyllabus(titleInput, descriptionInput, imageInput, categoryId)
+}
 
+function postSyllabus(title, description, image_url, category_id) {
+	//make image_url and category_id snakecase, even though it's JS => 
+  // confirm these values are coming through properly
+  console.log(title, description, image_url, category_id);
+  // build body object
+  let bodyData = {title, description, image_url, category_id}
+
+  fetch(endPoint, {
+	    // POST request
+	    method: "POST",
+	    headers: {"Content-Type": "application/json"},
+	    body: JSON.stringify(bodyData)
+	  })
+	  .then(response => response.json())
+	  .then(syllabus => {
+	    console.log(syllabus);
+	    const syllabusData = syllabus.data
+	    // render JSON response
+	    const syllabusMarkup = `
+	    <div data-id=${syllabus.id}>
+	      <img src=${syllabusData.attributes.image_url} height="200" width="250">
+	      <h3>${syllabusData.attributes.title}</h3>
+	      <p>${syllabusData.attributes.category.name}</p>
+	      <button data-id=${syllabusData.id}>edit</button>
+	    </div>
+	    <br><br>`;
+
+    document.querySelector('#syllabus-container').innerHTML += syllabusMarkup;
+  })
 }
 // get the data, then get access into the data 
 // catch get the errors from the controller 
